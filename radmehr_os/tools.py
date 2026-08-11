10de363a2aa214b0d970328dcb2ec8679a93f37a
Radmehr Farhadi <doctorzade@gmail.com>
2026-08-11 14:57:08 +0330

    create calculator

--- radmehr_os/tools.py
+++ radmehr_os/tools.py
@@ -1,2 +1,37 @@
 def Tools():
-    ...
+    print("1. 🧮Calculator")
+    print("2. exit")
+    whats_ = int(input("choose! choose!!"))
+    if whats_ == 1:
+        def c():
+            inputs = input("your: ").strip()
+            try:
+                x, y, z = inputs.split(" ")
+                y = str(y)
+                x = int(x)
+                z = int(z)
+            except ValueError:
+                print("Invalid! Example: 10 + 5")
+                c()
+                return
+        
+        
+            if y == "+":
+                anser = x + z
+
+            elif y == "-":
+                anser = x - z
+
+            elif y == "*":
+                anser = x * z
+
+            elif y == "/":
+                anser = x / z
+
+            print(f"{anser}")
+
+        c()
+            
+        
+    elif whats_ == 2:
+        return
