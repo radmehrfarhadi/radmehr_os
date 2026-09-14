859ec3337ee7219eef725784d0295fa900f356b2
Radmehr Farhadi <doctorzade@gmail.com>
2026-09-14 10:53:41 +0330

    update mode file

--- radmehr_os/mode.py
+++ radmehr_os/mode.py
@@ -1,12 +1,15 @@
 import bcrypt
 
 developer_mode = False
+
 def mode():
-    print("chenge to developer mode")
+    print("change to developer mode")
 
     hashed_password = b"$2b$12$xrSfcZ3fgg4AS.joH4cuye6b9qAQpsMeLiCiw1ZsMP8tmjkHjAvpy"
+
     password2 = input("password: ")
-    if bcrypt.checkpw(password2.encode(),hashed_password.encode()):
+
+    if bcrypt.checkpw(password2.encode(), hashed_password):
         return True
     else:
         print("Access denied!")
