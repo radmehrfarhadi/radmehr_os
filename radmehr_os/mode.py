ef4170900043efb9e1b6b636eee259ac85c20a45
Radmehr Farhadi <doctorzade@gmail.com>
2026-09-14 10:48:34 +0330

    update mode file

--- radmehr_os/mode.py
+++ radmehr_os/mode.py
@@ -4,7 +4,7 @@ developer_mode = False
 def mode():
     print("chenge to developer mode")
 
-    hashed_password = "$2b$12$xrSfcZ3fgg4AS.joH4cuye6b9qAQpsMeLiCiw1ZsMP8tmjkHjAvpy"
+    hashed_password = b"$2b$12$xrSfcZ3fgg4AS.joH4cuye6b9qAQpsMeLiCiw1ZsMP8tmjkHjAvpy"
     password2 = input("password: ")
     if bcrypt.checkpw(password2.encode(),hashed_password.encode()):
         return True
