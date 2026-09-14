efa25014ff78a99d840d9291e9197326e097d0d3
Radmehr Farhadi <doctorzade@gmail.com>
2026-09-14 10:59:12 +0330

    Fix developer mode password handling

--- radmehr_os/mode.py
+++ radmehr_os/mode.py
@@ -7,10 +7,14 @@ def mode():
 
     hashed_password = b"$2b$12$xrSfcZ3fgg4AS.joH4cuye6b9qAQpsMeLiCiw1ZsMP8tmjkHjAvpy"
 
-    password2 = input("password: ")
-
-    if bcrypt.checkpw(password2.encode(), hashed_password):
-        return True
-    else:
-        print("Access denied!")
+    try:
+        password2 = input("password: ")
+        if bcrypt.checkpw(password2.encode("utf-8"), hashed_password):
+            print("Developer mode enabled.")
+            return True
+        else:
+            print("Access denied!")
+            return False
+    except (ValueError, TypeError) as error:
+        print(f"Could not check developer password: {error}")
         return False
